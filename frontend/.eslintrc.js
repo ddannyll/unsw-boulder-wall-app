83747fdc15eb2ed4e83e5fa583336559dcd52a59
ddannyll <daniel.nguyen.s173@gmail.com>
2024-03-12 11:55:07 +1100

    init

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  plugins: ["unused-imports", "plugin:@react-three/recommended"],
+  extends: ["universe/native", "@react-three"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
